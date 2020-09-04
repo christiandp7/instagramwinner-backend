f2e80e36f2089dc9445c4d085bd9f3d55f46aab0
Christian <christiandp7@gmail.com>
2020-09-04 19:12:07 -0400

    Post Insert Many

--- src/models/Usuario.model.js
+++ src/models/Usuario.model.js
@@ -26,3 +26,8 @@ const userSchema = new Schema({
 }, { 
   timestamps: true,
 })
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
--- src/routes/usuarios.route.js
+++ src/routes/usuarios.route.js
@@ -1,17 +1,63 @@
 const { Router } = require('express');
-const Usuario = require('../models/Usuario.model')
+const User = require('../models/Usuario.model')
 const router = Router();
+
+// Controllers
 
 
 
 // Get All Usuarios with Pagination
-router.get('./users')
+router.get('./users', (req, res) => {
 
+  let from = req.query.from || 0;
+  from = Number(from);
 
-// Bulk users Add
-router.post('./users',)
+  let limit = req.query.limit || 10;
+  limit = Number(limit);
 
+  
+  User.find({  })
+      .skip(from)
+      .limit(limit)
+      .exec()
+      .then(users => {
+        User.count({ }, (err, counted) => {
+          res.json({
+            ok: true,
+            totalUsuariosBD: counted,
+            totalMostrados: users.length,
+            usuarios: users
+          })
+        })
+      }).catch(err => {
+        return res.status(400).json({
+          ok: false,
+          error: err
+        })
+      })
+})
 
+// Bulk users Add by HTML
+router.post('./users', (req, res) => {
+
+  let body = req.body;
+
+  User.collection.insertMany(body)
+      .then(newUsers => {
+        res.json({
+          ok: true,
+          nuevosUsuarios: newUsers
+        })
+      }).catch(err => {
+        return res.status(400).json({
+          ok: false,
+          error: err
+        })
+      })
+})
+
+// Delete Users By Id
+router.delete('./users/:id', )
 
 
 
