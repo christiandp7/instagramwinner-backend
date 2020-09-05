f64d879ba2be99e5e640550bd6c81cc143bcf8ca
Christian <christiandp7@gmail.com>
2020-09-05 19:35:52 -0400

    add json limit 50mb

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 // Middlewares
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(boolParser());
 
 //app.use(morgan('dev'))
